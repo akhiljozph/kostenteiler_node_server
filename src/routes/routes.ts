0a15980d0fabfa393579aa6edb7a752331374fa9
akhiljozph <ajpulluvelil@gmail.com>
2024-05-19 23:32:04 +0530

    KSTLR-11 - API implementation for login.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,6 +10,18 @@ const routes = (app: Application) => {
 
     app.post("/api/v1/user", validateResource(createUserSchema), createUserHandler);
 
+    /** 
+    * @swagger
+    * /api/v1/auth:
+    *   post:
+    *       summary: Create a login session
+    *       description: Verifies the user credentials and creates a valid login session.
+    *       schema:
+    *           type: string
+    *           responses:
+    *               200:
+    *               description: Successful response
+    */
     app.post("/api/v1/auth", validateResource(createLoginSessionSchema), createLoginSessionHandler);
 
     app.post("/api/v1/group", (req: Request, res: Response) => {
