71578707079965ad19fced5c85e88b0219f1751c
akhiljozph <ajpulluvelil@gmail.com>
2024-05-20 16:45:02 +0530

    KOSTLR-13 - Added swagger support for API documentation.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,19 +7,66 @@ import { createLoginSessionSchema } from "../schema/session.schema";
 import { createLoginSessionHandler } from "../controllers/session.controller";
 
 const routes = (app: Application) => {
+  /**
+   * @openapi
+   * '/api/v1/user':
+   *  post:
+   *     tags:
+   *     - User Controller
+   *     summary: Create a user
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *           schema:
+   *            type: object
+   *            required:
+   *              - fullName
+   *              - email
+   *              - username
+   *              - password
+   *            properties:
+   *              fullName:
+   *                type: string
+   *                default: John Doe
+   *              email:
+   *                type: string
+   *                default: johndoe@gmail.com
+   *              username:
+   *                type: string
+   *                default: johnDoe
+   *              password:
+   *                type: string
+   *                default: njD@052024
+   *     responses:
+   *      201:
+   *        description: Created
+   *      409:
+   *        description: Conflict
+   *      404:
+   *        description: Not Found
+   *      500:
+   *        description: Server Error
+   */
+  app.post(
+    "/api/v1/user",
+    validateResource(createUserSchema),
+    createUserHandler
+  );
 
-    app.post("/api/v1/user", validateResource(createUserSchema), createUserHandler);
+  app.post(
+    "/api/v1/auth",
+    validateResource(createLoginSessionSchema),
+    createLoginSessionHandler
+  );
 
-    app.post("/api/v1/auth", validateResource(createLoginSessionSchema), createLoginSessionHandler);
+  app.post("/api/v1/group", (req: Request, res: Response) => {
+    res.send("API for group creation");
+  });
 
-    app.post("/api/v1/group", (req: Request, res: Response) => {
-        res.send('API for group creation');
-    });
-
-    app.get("/api/v1/users", (req: Request, res: Response) => {
-        res.send('API for fetch all users');
-    });
-
+  app.get("/api/v1/users", (req: Request, res: Response) => {
+    res.send("API for fetch all users");
+  });
 };
 
 export default routes;
